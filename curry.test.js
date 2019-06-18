ec8eeeb686fa9e6da1844ff0cdf21bfe6f7be80a
Matthew Weeks <mweeks@tribalscale.com>
2019-06-18 17:51:39 -0400

    test: Add second test to assert multiple scenarios

--- curry.test.js
+++ curry.test.js
@@ -1,16 +1,26 @@
-const { test } = require('jest')
+// const { test } = require('jest')
 const assert = require('assert')
 
 const {curry} = require(".")
 
 
-test(`curry() should take a function and return a currified version of it`, assert => {
+test(`curry() should take a function and return a currified version of it`, () => {
     function myFunc (num1, num2, num3) {
       return num1 + num2 + num3
     }
     const myCurry = curry(myFunc)
     const actual = myCurry(1)(2)(3)
     const expected = 6
+
     assert.equal(actual, expected)
-    assert.end()
   })
+
+test(`curry() should take a function and return a currified version of it`, () => {
+  function myFunc (num1, num2) {
+    return num1 + num2 
+  }
+  const curryAdd = curry(myFunc)
+  const actual = curryAdd(1)(2)
+  const expected = 3
+  assert.equal(actual, expected)
+})
