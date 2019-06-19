571f501e41f0dc047cef3b379ee8a0f34b5b7e76
Jacob Zweifel <jacob@jacobzweifel.com>
2019-06-19 19:44:48 -0400

    test: add third test to help incrementally build solution

--- curry.test.js
+++ curry.test.js
@@ -1,26 +1,35 @@
 // const { test } = require('jest')
-const assert = require('assert')
+const assert = require("assert");
 
-const {curry} = require(".")
-
-
-test(`curry() should take a function and return a currified version of it`, () => {
-    function myFunc (num1, num2, num3) {
-      return num1 + num2 + num3
-    }
-    const myCurry = curry(myFunc)
-    const actual = myCurry(1)(2)(3)
-    const expected = 6
-
-    assert.equal(actual, expected)
-  })
+const { curry } = require(".");
 
 test(`curry() should take a function and return a currified version of it`, () => {
-  function myFunc (num1, num2) {
-    return num1 + num2 
+  function myFunc(num1, num2, num3) {
+    return num1 + num2 + num3;
   }
-  const curryAdd = curry(myFunc)
-  const actual = curryAdd(1)(2)
-  const expected = 3
-  assert.equal(actual, expected)
-})
+  const myCurry = curry(myFunc);
+  const actual = myCurry(1)(2)(3);
+  const expected = 6;
+
+  assert.equal(actual, expected);
+});
+
+test(`curry() should take a function and return a currified version of it`, () => {
+  function myFunc(num1, num2) {
+    return num1 + num2;
+  }
+  const curryAdd = curry(myFunc);
+  const actual = curryAdd(1)(2);
+  const expected = 3;
+  assert.equal(actual, expected);
+});
+
+test(`curry() should take a function of arity 1 and return it`, () => {
+  function myFunc(num1) {
+    return num1;
+  }
+  const myCurry = curry(myFunc);
+  const actual = myCurry(1);
+  const expected = 1;
+  assert.equal(actual, expected);
+});
