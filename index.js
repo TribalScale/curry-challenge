d7beae9a05b4e9dbb68863ed0f7171b9877e7be7
Serguey Martinez <sarellano85@gmail.com>
2019-06-18 21:04:34 -0400

    feat: your doughnuts make me go nuts!

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function curry () {
-
-}
-
 module.exports.curry = curry
+
+function curry (fn) {
+  let arity = fn.length
+  let args = []
+  return function curryAux (value) {
+    args.push(value)
+    return --arity ? curryAux : fn(...args)
+  }
+}
