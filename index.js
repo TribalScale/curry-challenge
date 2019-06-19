5a08da93808b2001c59588c939c51f1d07f4e87a
Jacob Zweifel <jacob@jacobzweifel.com>
2019-06-19 19:45:51 -0400

    feat: make  🍛

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function curry () {
-
+function curry(f) {
+  const makeCurry = (f, args) =>
+    args.length === f.length ? f(...args) : x => makeCurry(f, [...args, x]);
+  return makeCurry(f, []);
 }
 
-module.exports.curry = curry
+module.exports.curry = curry;
